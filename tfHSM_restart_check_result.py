ff1cd5a53d8679c579df9ecff2c58007ae939e8f
Pachaya Sailamul <pachaya_sailamul@brown.edu>
2017-06-27 17:55:20 -0400

    Initialization : 50 restarts

--- tfHSM_restart_check_result.py
+++ tfHSM_restart_check_result.py
@@ -1,5 +1,16 @@
 # load training of all restart then analyzed
-
+import numpy as np
+import tensorflow as tf
+import param
+from tf_HSM import tf_HSM
+import os
+import matplotlib.pyplot as plt
+from datetime import datetime
+import re 
+from tensorflow.python import debug 
+import time
+from visualization import *
+import sys
 
 
 def correlate_vectors(yhat_array, y_array):
@@ -15,86 +26,168 @@ def correlate_vectors(yhat_array, y_array):
 def log_likelihood(predictions,targets,epsilon =0.0000000000000000001):
   return tf.reduce_sum(predictions) - tf.reduce_sum(tf.mul(targets,tf.log(predictions + epsilon)))
 
-def hist_of_pred_and_record_response(pred_response, recorded_response, cell_id=0):
+def hist_of_pred_and_record_response(runcodestr, pred_response, recorded_response, cell_id=0, PLOT=False):
+  fig,ax = plt.subplots(figsize=(12,8))
   plt.subplot(121); plt.hist(recorded_response[:,cell_id]); plt.title('Recorded Response');
   plt.subplot(122); plt.hist(pred_response[:,cell_id]); plt.title('Predicted Response');
-  plt.suptitle("Distribution of cell #%g's response"%cell_id)
-  plt.show()
+  plt.suptitle("Code: %s\nDistribution of cell #%g's response"%(runcodestr,cell_id))
+  if(PLOT):
+   plt.show()
+  return fig,ax
 
-def plot_act_of_max_min_corr(yhat,train_set,corr):
+def plot_act_of_max_min_corr(runcodestr, yhat,train_set,corr, PLOT=False):
+    
     imax = np.argmax(corr) # note : actually have to combine neurons in all regions
-
+    fig_max =plt.figure()
     plt.plot(train_set[:,imax],'-ok')
     plt.plot(yhat[:,imax],'--or')
-    plt.title('Cell#%d has max corr of %f'%(imax+1,np.max(corr)))
-    plt.show()
+    plt.title('Code: %s\nCell#%d has max corr of %f'%(runcodestr,imax+1,np.max(corr)))
+    if(PLOT):
+      plt.show()
 
     imin = np.argmin(corr) # note : actually have to combine neurons in all regions
-
+    fig_min = plt.figure()
     plt.plot(train_set[:,imin],'-ok')
     plt.plot(yhat[:,imin],'--or')
-    plt.title('Cell#%d has min corr of %f'%(imin+1,np.min(corr)))
-    plt.show()
+    plt.title('Code: %s\nCell#%d has min corr of %f'%(runcodestr,imin+1,np.min(corr)))
+    if(PLOT):
+      plt.show()
 
+    return fig_max, fig_min
+
+        # check the training
+def plot_training_behav(runcodestr,loss_list,MSE_list,corr_list,activation_summary_lgn,activation_summary_l1,yhat_std,lr=1E-03,iterations=2500,PLOT=False):
+    itr_idx = range(iterations)
+
+    fig,ax = plt.subplots(figsize=(16,12))
+    plt.subplot(2, 3, 1)
+    plt.plot(itr_idx, loss_list, 'k-')
+    plt.title('Loss')
+    plt.xlabel('iterations')
+
+    plt.subplot(2, 3, 2)
+    plt.plot(itr_idx, MSE_list, 'b-')
+    plt.title('MSE')
+    plt.xlabel('iterations')
+
+    plt.subplot(2, 3, 3)
+    plt.plot(itr_idx, corr_list, 'r-')
+    plt.title('Mean Correlation')
+    plt.xlabel('iterations')
+
+    plt.subplot(2, 3, 4)
+    plt.plot(itr_idx, activation_summary_lgn, 'r-')
+    plt.title('Mean LGN activation')
+    plt.xlabel('iterations')
+
+    plt.subplot(2, 3, 5)
+    plt.plot(itr_idx, activation_summary_l1, 'r-')
+    plt.title('Mean L1 activation')
+    plt.xlabel('iterations')
+
+    plt.subplot(2,3, 6)
+    plt.plot(itr_idx, yhat_std, 'r-')
+    plt.title('std of predicted response')
+    plt.xlabel('iterations')
+
+    plt.suptitle("Code: %s lr=%.5f , itr = %g\n[%s]"%(runcodestr,lr,iterations,str(datetime.now())))
     
-    
-    #save
-    if(SAVEdat):
-        np.savez('%s/TRdat_%s.npz'%(summary_dir,summary_fname), 
-         TR_loss=loss_list, 
-         TR_mean_LGNact=activation_summary_lgn, 
-         TR_mean_L1act=activation_summary_l1, 
-         TR_std_pred_response =yhat_std, 
-         TR_MSE=MSE_list, 
-         TR_corr=corr_list,
-         TR_last_pred_response=yhat,
-         TR_last_l1_response=l1_response,
-         TR_last_lgn_response=lgn_response,
-         CONFIG=CONFIG)
-    
-    
-    # check the training
-    if(VISUALIZE):
-        itr_idx = range(iterations)
-        plt.subplot(2, 3, 1)
-        plt.plot(itr_idx, loss_list, 'k-')
-        plt.title('Loss')
-        plt.xlabel('iterations')
-
-        plt.subplot(2, 3, 2)
-        plt.plot(itr_idx, MSE_list, 'b-')
-        plt.title('MSE')
-        plt.xlabel('iterations')
-
-        plt.subplot(2, 3, 3)
-        plt.plot(itr_idx, corr_list, 'r-')
-        plt.title('Mean Correlation')
-        plt.xlabel('iterations')
-
-        plt.subplot(2, 3, 4)
-        plt.plot(itr_idx, activation_summary_lgn, 'r-')
-        plt.title('Mean LGN activation')
-        plt.xlabel('iterations')
-
-        plt.subplot(2, 3, 5)
-        plt.plot(itr_idx, activation_summary_l1, 'r-')
-        plt.title('Mean L1 activation')
-        plt.xlabel('iterations')
-
-        plt.subplot(2,3, 6)
-        plt.plot(itr_idx, yhat_std, 'r-')
-        plt.title('std of predicted response')
-        plt.xlabel('iterations')
-
-        plt.suptitle("Code: %s lr=%.5f , itr = %g\n[%s]"%(runcodestr,lr,iterations,str(datetime.now())))
+    if(PLOT):
         plt.show()
+    return fig, ax
 
-    if (PLOT_CORR_STATS):
-        pred_act = yhat; responses = train_set
-        #hist_of_pred_and_record_response(pred_act,responses)
+def get_trials_seednum(fullpath):
+  fullpath=fullpath.replace('.','_')
+  cutstr = re.split('_',fullpath)
+  matching = [s for s in cutstr if "trial" in s]
+  assert matching is not None
+  trial_num = int(matching[0][5:])
 
-        corr = computeCorr(yhat, train_set)
-        corr[np.isnan(corr)]=0.0
-        plot_act_of_max_min_corr(pred_act,responses,corr)
-        hist_of_pred_and_record_response(pred_act,responses,cell_id=np.argmax(corr))
-    print('Finished everything :: %s \n Time =%s '%(runcodestr,time.time() - tt_run_time))
+  matching = [s for s in cutstr if "seed" in s]
+  assert matching is not None
+  seed_num = int(matching[0][4:])
+  return trial_num, seed_num
+
+SAVEFIG = True
+
+def main(region_num='1', lr=1E-03, iterations=2500):
+    # Read file
+  if SAVEFIG :
+    date=str(datetime.now())
+    date = date[:10]
+    if not os.path.isdir('Figures/'+date+'/') :
+      os.mkdir('Figures/'+date+'/')
+    Fig_fold='Figures/'+date+'/'
+
+  dt_stamp = re.split(
+      '\.', str(datetime.now()))[0].\
+      replace(' ', '_').replace(':', '_').replace('-', '_')
+  
+  current_path = os.getcwd()+'/'
+  data_dir = os.path.join(  "TFtrainingSummary/Region"+region_num+'/')
+
+  all_folders = os.listdir(current_path+data_dir)
+  for sim_folder in all_folders:
+    sim_folder+='/'
+    directory = current_path+data_dir+sim_folder
+    print("------------------------------------------")
+    print(sim_folder)
+    print("------------------------------------------")
+    run_time = time.time()
+    #sim_folder ="AntolikRegion1_lr0.00100_itr2500_2017_06_23_06_39_51/"
+    for root, dirs, files in os.walk(directory): 
+
+      # Look inside each folder
+      matching = [file for file in files if file.endswith('.npz')]
+      if matching is None:
+        continue
+      fname=matching[0]
+      #import ipdb; ipdb.set_trace()
+      #fname = "TRdat_trained_HSM_3_trial50_seed50.npz"
+      fullpath = data_dir+sim_folder+fname
+      npz_dat = np.load(fullpath)
+        
+      trial_num, seed_num = get_trials_seednum(fullpath)
+
+      for k in npz_dat:
+          exec("{}".format(k)+"= npz_dat.f."+"{}".format(k))
+      CONFIG=CONFIG.all()
+      runcodestr = CONFIG['runcodestr']
+      train_set=np.load(current_path+'Data/region' + region_num+'/training_set.npy')
+      if(CONFIG['NORM_RESPONSE']):
+          train_set = train_set/(train_set.max()-train_set.min())
+          
+          
+      fig_train, ax=plot_training_behav(runcodestr = runcodestr,
+           loss_list=TR_loss,
+           MSE_list=TR_MSE,
+           corr_list=TR_corr,
+           activation_summary_lgn=TR_mean_LGNact,
+           activation_summary_l1=TR_mean_L1act,
+           yhat_std=TR_std_pred_response,
+           lr=lr, iterations=iterations)
+
+      responses = train_set
+      pred_act = TR_last_pred_response;
+      corr = computeCorr(pred_act, train_set)
+      corr[np.isnan(corr)]=0.0
+      fig_max,fig_min = plot_act_of_max_min_corr(runcodestr,pred_act,responses,corr)
+      fig_hist, ax = hist_of_pred_and_record_response(runcodestr,pred_act,responses,cell_id=np.argmax(corr))
+
+      if SAVEFIG:
+        sim_code="Region%s_lr%.5f_itr%g_trial%g_seed%g"%(region_num,lr,iterations,trial_num,seed_num)
+        fig_train.savefig(Fig_fold+sim_code+"_TrainRec.png")
+        fig_max.savefig(Fig_fold+sim_code+"_MaxCorr.png")
+        fig_min.savefig(Fig_fold+sim_code+"_MinCorr.png")
+        fig_hist.savefig(Fig_fold+sim_code+"_ResponseDist.png")
+
+      plt.close("all")      
+      print "Saved: Time %s\n" %(time.time() - run_time)
+
+if __name__ == "__main__":
+  region_num='3'
+  tt_run_time = time.time()
+  main(region_num=region_num)
+  print "Finished Region"+region_num+ ": Time %s\n" %(time.time() - tt_run_time)
+
+     
