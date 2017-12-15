d3287f36d0d88ba9bf7b1a867377f00b72c07dc4
Pachaya Sailamul <pachayasailamul@Pachayas-MacBook-Pro.local>
2017-12-14 23:43:55 -0500

    add path for local MBP

--- get_host_path.py
+++ get_host_path.py
@@ -4,8 +4,12 @@ def get_host_path(HOST=False,PATH=True):
         host='x7'; path='/home/pachaya/HSMmodel/'
     elif socket.gethostname() =='g13':
         host='g13'; path= '/home/pachaya/AntolikData/SourceCode/'
+    elif socket.gethostname() =='Pachayas-MacBook-Pro.local':
+        host='localMBP'; path= '/Users/pachayasailamul/Documents/HSMmodel/'       
     else:
-        raise Exception('Unknown Host : Please add your directory at get_path()')
+        #raise Exception('Unknown Host : Please add your directory at get_path()')
+        import os
+        host='Unknown'; path = os.getcwd() +'/'
     if(HOST&PATH):
         return host, path
     elif(HOST):
